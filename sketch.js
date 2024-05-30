f0c5c01d98a2e724f188037117cd43c8a05a1050
evethisi0901 <00001114762131sp@al.educacao.sp.gov.br>
2024-05-29 21:42:16 -0300

    adicionando arquivos js e css

--- sketch.js
+++ sketch.js
@@ -1,14 +1,15 @@
 function setup() {
-  createCanvas(600, 600);
-  background("black");
-}
- 
-function draw() {
-  stroke("red");
-  fill("pink");
-  
-  if(mouseIsPressed){
-    rect(mouseX, mouseY, 20, 35);
+    createCanvas(600, 600);
+    background("black");
+  }
+   
+  function draw() {
+    stroke("red");
+    fill("pink");
+    
+    if(mouseIsPressed){
+      rect(mouseX, mouseY, 20, 35);
+    }
+    
   }
   
-}
